d1219abae258cccf692c36f84d724a6ba5c7651e
Irurnnen <i19v199a56n78@gmail.com>
2025-05-24 18:36:09 +0300

    Move from sqlx to pgx

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,44 +1,50 @@
 package repository
 
 import (
+	"context"
+
 	_ "github.com/jackc/pgx/stdlib"
-	"github.com/jmoiron/sqlx"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
 )
 
 const DriverName = "pgx"
 
 type Repository struct {
-	db              *sqlx.DB
+	// db              *sqlx.DB
+	dbPool          *pgxpool.Pool
 	logger          *zap.Logger
 	HelloRepository HelloRepositoryInterface
 }
 
 func NewRepository(DSN string, logger *zap.Logger) (*Repository, error) {
-	db, err := sqlx.Connect(DriverName, DSN)
+	dbPool, err := pgxpool.New(context.Background(), DSN)
+	// db, err := sqlx.Connect(DriverName, DSN)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Repository{
-		db:              db,
+		// db:              db,
+		dbPool:          dbPool,
 		logger:          logger,
-		HelloRepository: NewHelloRepository(db, logger),
+		HelloRepository: NewHelloRepository(dbPool, logger),
 	}, nil
 }
 
-func NewRepositoryDB(db *sqlx.DB, logger *zap.Logger) *Repository {
+func NewRepositoryDB(dbPool *pgxpool.Pool, logger *zap.Logger) *Repository {
 	return &Repository{
-		db:              db,
+		// db:              db,
+		dbPool:          dbPool,
 		logger:          logger,
-		HelloRepository: NewHelloRepository(db, logger),
+		HelloRepository: NewHelloRepository(dbPool, logger),
 	}
 }
 
 func (r *Repository) Ping() error {
-	return r.db.Ping()
+	return r.dbPool.Ping(context.Background())
 }
 
-func (r *Repository) Close() error {
-	return r.db.Close()
+func (r *Repository) Close() {
+	r.dbPool.Close()
 }
--- internal/repository/repository_hello.go
+++ internal/repository/repository_hello.go
@@ -1,12 +1,14 @@
 package repository
 
 import (
-	"github.com/jmoiron/sqlx"
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
 )
 
 type HelloRepository struct {
-	db     *sqlx.DB
+	dbPool *pgxpool.Pool
 	logger *zap.Logger
 }
 
@@ -14,9 +16,9 @@ type HelloRepositoryInterface interface {
 	GetHelloMessage() (string, error)
 }
 
-func NewHelloRepository(db *sqlx.DB, logger *zap.Logger) *HelloRepository {
+func NewHelloRepository(dbPool *pgxpool.Pool, logger *zap.Logger) *HelloRepository {
 	return &HelloRepository{
-		db:     db,
+		dbPool: dbPool,
 		logger: logger,
 	}
 }
@@ -24,7 +26,7 @@ func NewHelloRepository(db *sqlx.DB, logger *zap.Logger) *HelloRepository {
 func (r *HelloRepository) GetHelloMessage() (string, error) {
 	var message string
 	query := "SELECT 'Hello World' AS message"
-	err := r.db.Get(&message, query)
+	err := r.dbPool.QueryRow(context.Background(), query).Scan(&message)
 	if err != nil {
 		zap.Error(err)
 		return "", err
